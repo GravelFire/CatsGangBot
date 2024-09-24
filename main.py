42b555e44442a5fad9337f14b4df767ff03c3cdb
Somet2mes <goonsomeway@gmail.com>
2024-09-24 18:44:25 +0800

    Update main.py to use global refer

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import asyncio
 from contextlib import suppress
+from bot.config import settings
 
 from bot.utils.launcher import process
 
+REFER_LOCK = asyncio.Lock()
 
 async def main():
-    await process()
+    try:
+        with open('refer.txt', 'r') as f:
+            GLOBAL_REFER = {line.strip():0 for line in f.readlines()}
+    except FileNotFoundError:
+        GLOBAL_REFER = {settings.REF_ID:0}
+        
+    await process(GLOBAL_REFER, REFER_LOCK)
+
 
 
 if __name__ == '__main__':
